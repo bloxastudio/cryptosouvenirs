21a62e95e3f73d796c587bba45af4d62b21a3ee1
Bence Puschmann <bence.puschmann@lombiq.com>
2022-05-19 22:16:50 +0200

    Overriding tokenURI to accept metadata with the correct file name an file extension.

--- blockchain/contracts/CryptoSouvenirs.sol
+++ blockchain/contracts/CryptoSouvenirs.sol
@@ -8,21 +8,29 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 error AlreadySold();
 
 contract CryptoSouvenirs is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
     string baseURI;
 
     constructor(
         string memory _name,
-        string memory _symbol
+        string memory _symbol,
+        string memory _initialBaseURI
     ) ERC721(_name, _symbol) {
-        
+        setBaseURI(_initialBaseURI);
     }
 
-    function mint(string memory _tokenURI) public payable {
-        uint256 supply = totalSupply();
-        uint256 tokenId = supply + 1;
+    function mint(uint256 _tokenId) public payable {
+        require(!_exists(_tokenId), "This NFT is already minted");
 
-        _safeMint(msg.sender, tokenId);
-        setBaseURI(_tokenURI);
+        _safeMint(msg.sender, _tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), ".json")) : "";
     }
 
    function _baseURI() internal view override returns (string memory) {
