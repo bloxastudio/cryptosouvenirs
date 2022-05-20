69e772bcf3f5c4b35f7bfc59d0995d042ed50eed
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-05-20 19:42:29 +0200

    Adding location related code

--- server/Cryptosouvenirs/Program.cs
+++ server/Cryptosouvenirs/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<GeoLocationOptions>(
+    builder.Configuration.GetSection("Geolocation"));
 
 var app = builder.Build();
 
