a4eb0eab66de43d0ec5e2eca42d67e3770dda911
Daniel Papilion <papiliond.94@gmail.com>
2022-05-21 23:18:02 +0200

    Add react-helmet-async for head tags management

--- client/src/views/Main/index.tsx
+++ client/src/views/Main/index.tsx
@@ -1,7 +1,1 @@
-import React from "react";
-
-export const Main = () => {
-  return <div>Main</div>;
-};
-
-export default Main;
+export { default } from "./main";
---
+++ client/src/views/Main/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Title } from "../../components/Head";
+
+export const Main = () => {
+  return (
+    <div>
+      <Title>Main</Title>
+      Main
+    </div>
+  );
+};
+
+export default Main;
