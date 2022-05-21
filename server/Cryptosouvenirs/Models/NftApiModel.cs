2331f0d34d5ba9626f15d6ba19227ff2fee9c710
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-05-21 22:10:08 +0200

    Code styling

--- server/Cryptosouvenirs/Models/NftApiModel.cs
+++ server/Cryptosouvenirs/Models/NftApiModel.cs
@@ -3,6 +3,6 @@ namespace Cryptosouvenirs.Models;
 public class NftApiModel
 {
     public string WalletId { get; set; }
-    public double Longitude { get; set; }
     public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
--- server/Cryptosouvenirs/Program.cs
+++ server/Cryptosouvenirs/Program.cs
@@ -3,10 +3,8 @@ using Cryptosouvenirs.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITableStorageService, TableStorageService>();
@@ -16,7 +14,6 @@ builder.Services.Configure<GeoLocationOptions>(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
