262d3a86e07c16dc5e2dc0283971002eded42def
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-05-21 23:37:07 +0200

    New api model

--- server/Cryptosouvenirs/Models/ApiModels.cs
+++ server/Cryptosouvenirs/Models/ApiModels.cs
@@ -1,8 +1,14 @@
 namespace Cryptosouvenirs.Models;
 
-public class NftApiModel
+public class AvailableNftApiModel
 {
     public string WalletId { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
+}
+
+public class CanBuyNftApiModel
+{
+    public string WalletId { get; set; }
+    public string NftId { get; set; }
 }
