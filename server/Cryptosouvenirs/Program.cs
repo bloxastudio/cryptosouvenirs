7b6ff74318c93229328051462d229b2e7197ab01
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-05-21 22:12:25 +0200

    Loading sample nfts in development

--- server/Cryptosouvenirs/Program.cs
+++ server/Cryptosouvenirs/Program.cs
@@ -1,3 +1,4 @@
+using Cryptosouvenirs.Constants;
 using Cryptosouvenirs.Models;
 using Cryptosouvenirs.Services;
 
@@ -18,6 +19,18 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using var scope = app.Services.CreateScope();
+    var tableStorageService = scope.ServiceProvider.GetService<ITableStorageService>();
+    await tableStorageService.UpsertEntityAsync(
+        Tables.Nft,
+        new NftEntity(Tables.Nft, "1") { Latitude = 47.497913, Longitude = 19.040236 });
+    await tableStorageService.UpsertEntityAsync(
+        Tables.Nft,
+        new NftEntity(Tables.Nft, "2") { Latitude = 40.730610, Longitude = -73.935242 });
+    await tableStorageService.UpsertEntityAsync(
+        Tables.Nft,
+        new NftEntity(Tables.Nft, "3") { Latitude = 35.652832, Longitude = 139.839478 });
 }
 
 app.UseHttpsRedirection();
@@ -26,4 +39,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
