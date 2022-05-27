61d1c35c59fc3000a3160f058caacdd0856b3817
Daniel Papilion <papiliond.94@gmail.com>
2022-05-28 01:51:42 +0200

    Display available NFTs on a leaflet map

---
+++ client/src/components/AvailableNFTs/MapView/mapView.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup, CircleMarker } from "react-leaflet";
+
+const MapView = (props: MapViewProps) => {
+  const { nfts, userPosition } = props;
+  const userCoords = [userPosition.coords.latitude + 0.01, userPosition.coords.longitude];
+
+  return (
+    <MapContainer center={userCoords} zoom={13} scrollWheelZoom={false} style={{ height: "100vh", width: "100wh" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker key={0} position={userCoords}></Marker>
+      {nfts.map(({ latitude, longitude, rowKey }) => (
+        <CircleMarker key={rowKey} center={[latitude, longitude]}>
+          <Popup>{"Mint " + rowKey}</Popup>
+        </CircleMarker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export interface MapViewProps {
+  nfts: Array<AvailableNFT>;
+  userPosition: GeolocationPosition;
+}
+
+export default MapView;
--- client/src/components/AvailableNFTs/availableNFTs.tsx
+++ client/src/components/AvailableNFTs/availableNFTs.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Stack, Text, Button, Table, Title, Modal } from "@mantine/core";
+import { Globe as GlobeIcon, Table as TableIcon } from "tabler-icons-react";
+import { Stack, Text, Button, Table, Title, Modal, Container, Group, ActionIcon, Grid, Paper } from "@mantine/core";
 import { useAvailableNFTs } from "@hooks/useAvailableNFTs";
 import { AppState } from "@redux/store";
 import Mint from "@components/Mint";
+import MapView from "./MapView/mapView";
 
 const AvailableNFTs = () => {
   const position = useSelector((state: AppState) => state.data.position);
   const { data, loading, getAvailableNFTs } = useAvailableNFTs({ position });
   const [selectedNFT, setSelectedNFT] = useState<AvailableNFT | null>(null);
+  const [viewType, setViewType] = useState<ViewType>("map");
 
   if (!position) {
     return null;
@@ -29,37 +32,60 @@ const AvailableNFTs = () => {
           {"Show available NFTs!"}
         </Button>
       )}
-      {data && data.length ? (
+      {data && (
         <>
-          <Title order={2}>{"NFTs in you area"}</Title>
-          <Table highlightOnHover>
-            <thead>
-              <tr>
-                <th>{"Id"}</th>
-                <th>{"Latitude"}</th>
-                <th>{"Longitude"}</th>
-                <th>{""}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((availableNFT, index) => {
-                const { rowKey, latitude, longitude } = availableNFT;
-                return (
-                  <tr key={index}>
-                    <td>{rowKey}</td>
-                    <td>{latitude}</td>
-                    <td>{longitude}</td>
-                    <td>
-                      <Button size="sm" variant="light" onClick={() => setSelectedNFT(availableNFT)}>
-                        {"Mint"}
-                      </Button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
+          <Grid>
+            <Grid.Col span={6}>
+              <Title order={2}>{"NFTs in you area"}</Title>
+            </Grid.Col>
+            <Grid.Col span={6} style={{ display: "flex", justifyContent: "flex-end" }}>
+              <ActionIcon
+                color={"gray"}
+                onClick={() => setViewType((curr) => (curr === "map" ? "table" : "map"))}
+                title={viewType === "map" ? "View list" : "View map"}
+              >
+                {viewType === "map" ? <TableIcon /> : <GlobeIcon />}
+              </ActionIcon>
+            </Grid.Col>
+          </Grid>
         </>
+      )}
+      {data && data.length ? (
+        viewType === "table" ? (
+          <>
+            <Table highlightOnHover>
+              <thead>
+                <tr>
+                  <th>{"Id"}</th>
+                  <th>{"Latitude"}</th>
+                  <th>{"Longitude"}</th>
+                  <th>{""}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((availableNFT, index) => {
+                  const { rowKey, latitude, longitude } = availableNFT;
+                  return (
+                    <tr key={index}>
+                      <td>{rowKey}</td>
+                      <td>{latitude}</td>
+                      <td>{longitude}</td>
+                      <td>
+                        <Button size="sm" variant="light" onClick={() => setSelectedNFT(availableNFT)}>
+                          {"Mint"}
+                        </Button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </>
+        ) : (
+          <div>
+            <MapView nfts={data} userPosition={position} />
+          </div>
+        )
       ) : (
         data && <Text>{"No NFTs in you area."}</Text>
       )}
@@ -69,5 +95,7 @@ const AvailableNFTs = () => {
     </Stack>
   );
 };
+
+export type ViewType = "map" | "table";
 
 export default AvailableNFTs;
