6ba0d717362ea7394cf7009c85299a18dfb1aaa7
Daniel Papilion <papiliond.94@gmail.com>
2022-05-27 19:10:57 +0200

    Add and configure axios

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,16 @@ import { HelmetProvider } from "react-helmet-async";
 import store from "@redux/store";
 import App from "./App";
 
+import Axios from "axios";
+import { configure } from "axios-hooks";
+
+const axios = Axios.create({
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  withCredentials: false,
+});
+
+configure({ axios });
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
